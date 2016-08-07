6b87be523b0ffa5a5ae253255f7f4e4742de33d6
tomtc123 <997063802@qq.com>
2016-08-07 17:49:45 +0800

    create

---
+++ Assets/Script/TableManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Reflection;
+
+public class TableManager {
+
+    SqliteConnection mSqliteConnection = null;
+    private static TableManager mInstance;
+    public static TableManager Instance
+    {
+        get
+        {
+            if (mInstance == null)
+            {
+                mInstance = new TableManager();
+                mInstance.LoadDB();
+            }            
+            return mInstance;
+        }
+    }
+
+    public void LoadDB()
+    {
+        float time = Time.realtimeSinceStartup;
+        if (mSqliteConnection != null)
+        {
+            mSqliteConnection.Close();
+            mSqliteConnection = null;
+        }
+        mSqliteConnection = new SqliteConnection("data source=test.db");
+        mSqliteConnection.Open();
+        Debug.LogFormat("open time={0}s", Time.realtimeSinceStartup - time);
+    }
+
+    public T GetTableData<T>(eTableName table, string id)
+    {
+        T data = System.Activator.CreateInstance<T>();
+        float time = Time.realtimeSinceStartup;
+        SqliteCommand cmd = mSqliteConnection.CreateCommand();
+        cmd.CommandText = string.Format("select * from {0} where id={1}", table.ToString(), id);
+        SqliteDataReader reader = cmd.ExecuteReader();
+        Debug.LogFormat("search time={0}s", Time.realtimeSinceStartup - time);
+        if (reader.HasRows)
+        {
+            FieldInfo[] fields = data.GetType().GetFields();
+            foreach (var item in fields)
+            {
+                int index = reader.GetOrdinal(item.Name);
+                object v = reader.GetValue(index);
+                if (v != null)
+                    item.SetValue(data, v);
+            }
+            return data;
+        }
+        return data;
+    }
+
+
+}
